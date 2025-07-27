798d1b0a477d3f750d6ead2413389c2a5e13f59e
Sachintha-Lakruwan <sachi.new2002@gmail.com>
2025-07-27 20:07:42 +0530

    Develop best products section

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,16 +1,34 @@
+import Image from "next/image";
 import React from "react";
+import { ArrowUpRight } from "lucide-react";
 
 interface CardProps {
   title: string;
-  price: string;
+  price: number;
   image: string;
 }
 
-export default function Card({ title, price, image }: CardProps) {
+export default function Card({ title, image, price }: CardProps) {
   return (
-    <div className=" w-full h-full bg-white rounded-lg shadow-md">
-      <h2 className=" text-2xl font-bold">{title}</h2>
-      <p className=" text-sm text-gray-500">{price}</p>
-    </div>
+    <li className=" w-full">
+      <div className=" w-full aspect-[4/5] relative rounded-2xl overflow-hidden bg-neutral-200">
+        <Image
+          src={`/products/${image}`}
+          alt={title}
+          fill
+          className=" object-cover"
+        />
+      </div>
+      <div className=" flex flex-col gap-2 mt-3">
+        <h4 className=" text-xl font-bold">{title}</h4>
+        <p className=" text-sm text-gray-500 italic">${price}</p>
+        <p className=" text-amber-500 text-xl mt-3 flex flex-row gap-2 items-cente">
+          Buy
+          <span>
+            <ArrowUpRight />
+          </span>
+        </p>
+      </div>
+    </li>
   );
 }
